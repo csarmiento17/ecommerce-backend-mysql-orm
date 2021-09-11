e7ac886021e5518362c6879be8ab5963e5890f5f
jsarmiento17 <jay@stonewellfinancial.com>
2021-09-11 04:20:17 -0400

    Created the Models and Associations

--- models/index.js
+++ models/index.js
@@ -1,16 +1,30 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
@@ -18,3 +32,14 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+// GIVEN a functional Express.js API
+
+// WHEN I enter schema and seed commands
+// THEN a development database is created and is seeded with test data
+// WHEN I enter the command to invoke the application
+// THEN my server is started and the Sequelize models are synced to the MySQL database
+// WHEN I open API GET routes in Insomnia Core for categories, products, or tags
+// THEN the data for each of these routes is displayed in a formatted JSON
+// WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+// THEN I am able to successfully create, update, and delete data in my database
